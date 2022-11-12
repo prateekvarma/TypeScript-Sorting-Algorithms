32c7dbafdf6f6c5c4a29afe71821a5a33574dde5
Prateek Varma <prtk.varma@gmail.com>
2022-11-11 22:39:36 -0600

    Extracting Sorter function to it's own class (inc)

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,4 @@
-class Sorter {
-    collection: number[] | string;
 
-    constructor(collection: number[] | string) {
-        this.collection = collection;
-    }
-
-    sort(): void {
-        const { length } = this.collection;
-
-        for(let i=0 ; i<length ; i++) {
-            for(let j=0 ; j<length-i-1 ; j++) {
-                //This if condition applies only to collections that are of type number[]
-                if (this.collection instanceof Array) { 
-                    if (this.collection[j] > this.collection[j+1]) {
-                        const temp = this.collection[j];
-                        this.collection[j] = this.collection[j+1];
-                        this.collection[j+1] = temp;
-                    }
-                }
-                
-                //This if condition applies only to collections that are of type string
-                if (this.collection instanceof String) {
-                    
-                }
-            }
-        }
-    }
-}
 
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
