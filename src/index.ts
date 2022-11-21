634e32ebcb36a87d5381e157588b56331727e82c
Prateek Varma <prtk.varma@gmail.com>
2022-11-21 16:38:07 -0600

    Used inheritance on CharactersCollection & LinkedList and tested them out from index

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,29 @@ import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
-numbersCollection.sort();
-console.log("Result: ", numbersCollection.data);
+//Below, code is according to Inheritance and Abstraction
+// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+// numbersCollection.sort();
+// console.log("Result: ", numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('Xaasy');
 // const sorter = new Sorter(charactersCollection);
 // sorter.sort();
 // console.log(charactersCollection.data);
 
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-10);
-// linkedList.add(-3);
-// linkedList.add(4);
+//Below, code is according to Inheritance and Abstraction
+// const charactersCollection = new CharactersCollection('Xaasy');
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
+
+//Below, code is according to Inheritance and Abstraction
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
 
 // const sorter = new Sorter(linkedList);
 // sorter.sort();
